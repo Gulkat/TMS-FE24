93eb8cc5f7c4dd5d659d903d264c3ffb42caae18
Gulkat <Kateguleshova@gmail.com>
2022-03-31 18:38:16 +0300

    Готовый макет на проверку

---
+++ dist/index.09c24910.js
@@ -0,0 +1,27 @@
+const swiper = new Swiper('.swiper', {
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        type: 'bullets'
+    },
+    slidesPerView: 3,
+    spaceBetween: 30,
+    // slidesPerGroup: 3,
+    speed: 800,
+    mousewheel: {
+        invert: false
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2
+        },
+        992: {
+            slidesPerView: 3
+        }
+    }
+});
+
+//# sourceMappingURL=index.09c24910.js.map
